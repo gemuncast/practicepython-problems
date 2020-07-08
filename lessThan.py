2bb1f6a10fc8ebb6f7f4ad3b415a3eb6a0f51d4f
gemuncast <gemuncast@gmail.com>
2020-07-08 08:54:33 -0400

    less than problem plus extras

---
+++ lessThan.py
@@ -0,0 +1,34 @@
+#List Less Than Ten
+#Take a list, say for example this one: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#and write a program that prints out all the elements of the list that are less than 10.
+#Extras
+# 1- Instead of printing the elements one by one, make a new list that has all the 
+# elements less than 5 from this list in it and print out this new list.
+# 2- Write this in one line of Python.
+# 3- Ask the user for a number and return a list that contains only elements from the 
+# original list a that are smaller than that number given by the user.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for i in a:
+    if i < 10 :
+        print(i)
+
+#1
+
+new_list =[]
+
+for i in a:
+    if i < 10 :
+        new_list.append(i)
+
+print(new_list)
+
+#2
+#one_line_array = [i for i in a if i<10]
+#print(one_line_array)
+print(f"{[i for i in a if i<10]}")
+
+#3
+num = int(input("enter number: "))
+print(f"{[i for i in a if i< num]}")
